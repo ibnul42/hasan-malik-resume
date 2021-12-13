fe48142277dca2c5111bb4717012f7cf0bf56dfd
Ibnul Ashir <ibnulashir42@gmail.com>
2021-12-13 17:37:35 +0600

    icons changed

--- src/App.js
+++ src/App.js
@@ -8,41 +8,50 @@ import "./app.css";
 import Project from "./components/Project";
 import Skill from "./components/Skill";
 import Certification from "./components/Certification";
+import image1 from './images/Flag-1.png'
+import image2 from './images/Flag-2.png'
+import image3 from './images/Flag-3.png'
+import image4 from './images/Flag-4.png'
 
 const languages = [
   {
     code: "en",
     name: "English",
     country_code: "gb",
+    image: image1
   },
   {
     code: "kz",
     name: "қазақ",
     country_code: "kz",
+    image: image2
   },
   {
     code: "rs",
     name: "Россия",
     country_code: "rs",
+    image: image3
   },
   {
     code: "kd",
     name: "Kurdî",
     country_code: "iq",
+    image: image4
   },
 ];
 
-const GlobeIcon = ({ width = 24, height = 24 }) => (
+const GlobeIcon = ({ width = 20, height = 20 }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={width}
     height={height}
     fill="currentColor"
-    className="bi bi-globe"
+    className="bi bi-translate"
     viewBox="0 0 16 16"
-    // style={{position: "fixed"}}
+    color="black"
   >
-    <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h3.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h3.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h3.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12h3.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h3.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h3.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h3.355z" />
+    <path d="M4.545 6.714 4.11 8H3l1.862-5h1.284L8 8H6.833l-.435-1.286H4.545zm1.634-.736L5.5 3.956h-.049l-.679 2.022H6.18z" />
+    <path d="M0 2a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v3h3a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-3H2a2 2 0 0 1-2-2V2zm2-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H2zm7.138 9.995c.193.301.402.583.63.846-.748.575-1.673 1.001-2.768 1.292.178.217.451.635.555.867 1.125-.359 2.08-.844 2.886-1.494.777.665 1.739 1.165 2.93 1.472.133-.254.414-.673.629-.89-1.125-.253-2.057-.694-2.82-1.284.681-.747 1.222-1.651 1.621-2.757H14V8h-3v1.047h.765c-.318.844-.74 1.546-1.272 2.13a6.066 6.066 0 0 1-.415-.492 1.988 1.988 0 0 1-.94.31z" />
   </svg>
 );
 
@@ -68,49 +77,56 @@ function App() {
           >
             <GlobeIcon />
           </button>
-          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-            {languages.map(({ code, name, country_code }) => (
-              <li key={code}>
-                <button
-                  className="dropdown-item"
-                  onClick={() => i18next.changeLanguage(code)}
-                  disabled={code === curreLanguageCode}
-                >
-                  <span
-                    className={`flag-icon flag-icon-${country_code} mx-2`}
-                    style={{ opacity: code === curreLanguageCode ? 0.5 : 1 }}
-                  ></span>
-                  {name}
-                </button>
-              </li>
-            ))}
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1" style={{padding: '0rem 0'}}>
+            {languages.map(({ code, name, country_code, image }) => {
+              console.log(image);
+              return (
+                <li key={code}>
+                  
+                  <button
+                    className="dropdown-item"
+                    onClick={() => i18next.changeLanguage(code)}
+                    disabled={code === curreLanguageCode}
+                  >
+                    <img src={image} alt="" className="px-2"  />
+                    {/* <span
+                      className={`flag-icon flag-icon-${country_code} mx-2`}
+                      style={{ opacity: code === curreLanguageCode ? 0.5 : 1 }}
+                    ></span> */}
+                      {name}
+                  </button>
+                </li>
+              )
+            })}
           </ul>
         </div>
       </div>
       <Navbar />
       <div className="d-flex flex-column align-items-start mt-2">
-        <h3 className="fw-bold">{t('hello')}</h3>
+        <h3 className="fw-bold">{t("hello")}</h3>
         <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
           laboriosam.
         </p>
 
         <div className="my-2">
-          <h3 className="fw-bold">{t('projects')}</h3>
+          <h3 className="fw-bold">{t("projects")}</h3>
           <Project />
         </div>
 
         <div className="my-2">
-          <h3 className="fw-bold">{t('skills')}</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi recusandae, officiis reprehenderit veritatis ipsum id.</p>
+          <h3 className="fw-bold">{t("skills")}</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+            recusandae, officiis reprehenderit veritatis ipsum id.
+          </p>
           <Skill />
         </div>
 
         <div className="my-2">
-          <h3 className="fw-bold">{t('certificates')}</h3>
+          <h3 className="fw-bold">{t("certificates")}</h3>
           <Certification />
         </div>
-
       </div>
     </div>
   );
